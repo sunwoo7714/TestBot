8bf9648a6ad0aad7b3f88c481057154e75804e3e
sunwoo7714 <bswihihi@naver.com>
2025-08-12 20:03:00 +0900

    Add files via upload

---
+++ b.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import requests
+
+TOKEN = 'MTQwNDc0OTcwODYyNjM2NjQ5Ng.G8epOZ.CuZ5TqjiGMfBGbXdGn_kYnkxTGXAGwy1bdToSw'
+API_URL = 'http://127.0.0.1:5000/announce'
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.command()
+async def 공지(ctx, *, 내용):
+    data = {"message": 내용}
+    try:
+        response = requests.post(API_URL, json=data)
+        if response.status_code == 200:
+            await ctx.send("인게임 공지가 성공적으로 전송되었어요!")
+        else:
+            await ctx.send("공지 전송에 실패했어요.")
+    except Exception as e:
+        await ctx.send(f"오류 발생: {e}")
+
+@bot.event
+async def on_ready():
+    print(f'봇 로그인 성공: {bot.user}')
+
+bot.run(TOKEN)
